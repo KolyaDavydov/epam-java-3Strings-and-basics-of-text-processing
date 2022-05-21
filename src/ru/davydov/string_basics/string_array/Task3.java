cad8d71fab989842cc59924ee3d2661e76b51feb
Kolya Davydov <kolya.davydov@inbox.ru>
2022-05-21 17:11:26 +0300

    added Task3 to string_array

---
+++ src/ru/davydov/string_basics/string_array/Task3.java
@@ -0,0 +1,20 @@
+package ru.davydov.string_basics.string_array;
+
+/*
+ * 3. В строке найти количество цифр.
+ * */
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		
+		String str = "12 34fj5 6gjdkh7 8,90";
+		
+		str = str.replaceAll("\\D", "");
+		
+		int numOfDigit = str.length();
+		
+		System.out.println("Количество цифр в строке: " + numOfDigit);
+	}
+
+}
