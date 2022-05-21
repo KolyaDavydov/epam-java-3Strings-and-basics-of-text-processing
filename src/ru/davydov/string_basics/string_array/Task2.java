3cb93855b5f7d9ae153e7ef08f01d24b9c566b90
Kolya Davydov <kolya.davydov@inbox.ru>
2022-05-21 16:33:27 +0300

    added Task2 to string_array

---
+++ src/ru/davydov/string_basics/string_array/Task2.java
@@ -0,0 +1,19 @@
+package ru.davydov.string_basics.string_array;
+
+/*
+ * 2. Замените в строке все вхождения 'word' на 'letter'.
+ * */
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		
+		String str = "Word is not world, Word consist word. wordMe";
+		System.out.println(str);
+		
+		str = str.replace("word", "letter");
+		System.out.println(str);
+
+	}
+
+}
